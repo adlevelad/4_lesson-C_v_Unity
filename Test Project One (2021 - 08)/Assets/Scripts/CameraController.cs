ec5065a6f9f4cc89580444ead31969e9899d9689
adlevelad <adlevel@yandex.ru>
2021-08-09 03:06:00 +0300

    Исправление управления
    Исправил управление - слежение камеры, поворот камеры, изменил скрипт
    Добавил прыжок

--- Test Project One (2021 - 08)/Assets/Scripts/CameraController.cs
+++ Test Project One (2021 - 08)/Assets/Scripts/CameraController.cs
@@ -32,10 +32,10 @@ namespace TestProjectOne
             //_offset.x = Input.GetAxis("Horizontal");
             //_offset.z = Input.GetAxis("Vertical");
 
-            //var speed = _offset * (_speedRotation * Time.deltaTime);
-            //transform.Translate(speed);
-            transform.Rotate(Vector3.up * (_speedRotation * Input.GetAxis("Mouse X") * Time.deltaTime)); // поворот камеры по оси X
-            transform.Rotate(Vector3.left * (_speedRotation * Input.GetAxis("Mouse Y") * Time.deltaTime)); // поворот камеры по оси Y
+            // var speed = _offset * (_speedRotation * Time.deltaTime);
+            // transform.Translate(speed);
+            // transform.Rotate(Vector3.up * (_speedRotation * Input.GetAxis("Mouse X") * Time.deltaTime)); // поворот камеры по оси X
+            // transform.Rotate(Vector3.left * (_speedRotation * Input.GetAxis("Mouse Y") * Time.deltaTime)); // поворот камеры по оси Y
         }
     }
 }
--- Test Project One (2021 - 08)/Assets/Scripts/Player.cs
+++ Test Project One (2021 - 08)/Assets/Scripts/Player.cs
@@ -7,9 +7,12 @@ namespace TestProjectOne
 {
     public class Player : MonoBehaviour
     {
-        private float _strange = 3.0f;
-        
+        [SerializeField, Range(1, 50)] private float _strangeJump = 3.0f;
+        [SerializeField, Range(1, 10)] private float _speedPlayer = 3;
+        [SerializeField, Range(1, 50)] private float _sensitivity = 25;
+        private Vector3 _vector3;
         private Rigidbody _rigidbody;
+        private bool _isGround = true;
 
         private void Awake()
         {
@@ -18,18 +21,30 @@ namespace TestProjectOne
 
         public void Start()
         {
-            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody = GetComponent<Rigidbody>(); // инициализируем компонент твердого тела для физики
         }
 
         protected void Move()
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            if (Input.GetKeyDown(KeyCode.Space)) _rigidbody.AddForce(transform.up * _strange, ForceMode.Impulse); // прыгать
+            // проверка позиции плеера на поверхности
+            RaycastHit hit;
+            var rayCast = Physics.Raycast(transform.GetChild(0).position, Vector3.down, out hit, 8.5f);
+            // if (rayCast)
+            //     Debug.Log(hit.collider.gameObject.name);
+            if (rayCast) _isGround = true;
+            else _isGround = false;
+            // прыжок (сила прыжка зависит от веса тела RigidBody и заданной силы прыжка)
+            if (Input.GetKeyDown(KeyCode.Space) && _isGround) 
+                _rigidbody.AddForce(transform.up * _strangeJump, ForceMode.Impulse);
 
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            _rigidbody.AddForce(movement * _strange);
-            
+            // управление клавишами и поворот камеры мышью
+            _vector3.x = Input.GetAxis("Horizontal");
+            _vector3.z = Input.GetAxis("Vertical");
+            transform.Rotate(Vector3.up * (_sensitivity * Input.GetAxis("Mouse X") * Time.deltaTime));
+
+            // реализиция передвижения персонажа с заданной скоростью
+            var speed = _vector3 * (_speedPlayer * Time.deltaTime);
+            transform.Translate(speed);
         }
     }
 }
